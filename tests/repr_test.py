e8fdb74f70889b93532280d25b44b9b0b5f9ca90
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2020-06-27 22:01:50 +0200

    test(core): update from_pandas repr case to cover new dtypes

--- tests/repr_test.py
+++ tests/repr_test.py
@@ -73,9 +73,10 @@ def test_repr_from_pandas():
     dd_dict = {
         'boolean': [True, True, False, None, True],
         'text': ['This', 'is', 'some', 'text', 'so...'],
-        'numbers_1': [1, 30, -2, 1.5, 0.000],
-        'numbers_2': [1, None, -2, 1.5, 0.000],
-        'numbers_3': [1, np.nan, -2, 1.5, 0.000],
+        'text_missing': pd.Series(['Some', 'parts', None, 'missing', None], dtype='string'),
+        'float': [1, 30, -2, 1.5, 0.000],
+        'float_missing': [1, None, -2, 1.5, 0.000],
+        'int_missing': pd.Series([1, None, 5, 1, 10], dtype='Int64'),
         'datetime_1': [pd.NaT, datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1)],
         'datetime_2': [pd.NaT, None, pd.NaT, pd.NaT, pd.NaT],
         'datetime_3': [pd.Timedelta('1M'), pd.Timedelta('1D'), pd.Timedelta('100M'), pd.Timedelta('2D'), pd.Timedelta('1H')],
@@ -85,10 +86,21 @@ def test_repr_from_pandas():
     }
 
     # Get pandas dataframe
-    dd = pd.DataFrame(dd_dict)
-    dd['datetime_7'] = pd.to_timedelta(dd['datetime_2'] - dd['datetime_1'])
-    ds = vaex.from_pandas(dd)
-    repr_value = repr(ds)
-    str_value = str(ds)
+    pandas_df = pd.DataFrame(dd_dict)
+    pandas_df['datetime_7'] = pd.to_timedelta(pandas_df['datetime_2'] - pandas_df['datetime_1'])
+    vaex_df = vaex.from_pandas(pandas_df)
+    repr_value = repr(vaex_df)
+    str_value = str(vaex_df)
+
     assert 'NaT' in repr_value
     assert 'NaT' in str_value
+    assert '--' in repr_value
+    assert '--' in str_value
+
+    assert vaex_df.text_missing.is_masked == True
+    assert vaex_df.int_missing.is_masked == True
+    assert vaex_df.float_missing.is_masked == False
+    assert vaex_df.int_missing.tolist() == [1, None, 5, 1, 10]
+    assert vaex_df.text_missing.tolist() == ['Some', 'parts', None, 'missing', None]
+    assert vaex_df.float_missing.values[[0, 2, 3, 4]].tolist() == [1.0, -2.0, 1.5, 0.0]
+    assert np.isnan(vaex_df.float_missing.values[1])
